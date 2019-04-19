43439af72855f87cf29f7eb2a3bbbf0974cfd673
Dmitry Zolotukhin <zlogic@gmail.com>
2019-04-19 21:01:09 +0200

    Disabled caching for the API.

--- server/router.go
+++ server/router.go
@@ -10,6 +10,14 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+// NoCacheHeaderMiddlewareFunc creates a handler to disable caching.
+func NoCacheHeaderMiddlewareFunc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "private")
+		next.ServeHTTP(w, r)
+	})
+}
+
 func registrationAllowed() bool {
 	allowRegistrationStr, ok := os.LookupEnv("ALLOW_REGISTRATION")
 	var allowRegistration bool
@@ -29,35 +37,36 @@ func CreateRouter(s *Services) (*mux.Router, error) {
 	registrationAllowed := registrationAllowed()
 
 	r := mux.NewRouter()
-	r.HandleFunc("/", RootHandler(s)).Methods("GET")
-	r.HandleFunc("/login", HTMLLoginHandler(s)).Methods("GET")
+	r.HandleFunc("/", RootHandler(s)).Methods(http.MethodGet)
+	r.HandleFunc("/login", HTMLLoginHandler(s)).Methods(http.MethodGet)
 	if registrationAllowed {
-		r.HandleFunc("/register", HTMLRegisterHandler(s)).Methods("GET")
+		r.HandleFunc("/register", HTMLRegisterHandler(s)).Methods(http.MethodGet)
 	}
-	r.HandleFunc("/logout", LogoutHandler(s)).Methods("GET")
-	r.HandleFunc("/transactions", HTMLUserPageHandler(s)).Methods("GET").Name("transactions")
-	r.HandleFunc("/transactioneditor", HTMLUserPageHandler(s)).Methods("GET").Name("transactioneditor")
-	r.HandleFunc("/report", HTMLUserPageHandler(s)).Methods("POST").Name("report")
-	r.HandleFunc("/accounts", HTMLUserPageHandler(s)).Methods("GET").Name("accounts")
-	r.HandleFunc("/accounteditor", HTMLUserPageHandler(s)).Methods("GET").Name("accounteditor")
-	r.HandleFunc("/settings", HTMLUserPageHandler(s)).Methods("GET").Name("settings")
+	r.HandleFunc("/logout", LogoutHandler(s)).Methods(http.MethodGet)
+	r.HandleFunc("/transactions", HTMLUserPageHandler(s)).Methods(http.MethodGet).Name("transactions")
+	r.HandleFunc("/transactioneditor", HTMLUserPageHandler(s)).Methods(http.MethodGet).Name("transactioneditor")
+	r.HandleFunc("/report", HTMLUserPageHandler(s)).Methods(http.MethodPost).Name("report")
+	r.HandleFunc("/accounts", HTMLUserPageHandler(s)).Methods(http.MethodGet).Name("accounts")
+	r.HandleFunc("/accounteditor", HTMLUserPageHandler(s)).Methods(http.MethodGet).Name("accounteditor")
+	r.HandleFunc("/settings", HTMLUserPageHandler(s)).Methods(http.MethodGet).Name("settings")
 	r.HandleFunc("/favicon.ico", FaviconHandler)
 	fs := http.FileServer(staticResourceFileSystem{http.Dir("static")})
 	r.PathPrefix("/static/").Handler(http.StripPrefix(strings.TrimRight("/static", "/"), fs))
 
 	api := r.PathPrefix("/api").Subrouter()
-	api.HandleFunc("/login", LoginHandler(s)).Methods("POST")
+	api.Use(NoCacheHeaderMiddlewareFunc)
+	api.HandleFunc("/login", LoginHandler(s)).Methods(http.MethodPost)
 	if registrationAllowed {
-		api.HandleFunc("/register", RegisterHandler(s)).Methods("POST")
+		api.HandleFunc("/register", RegisterHandler(s)).Methods(http.MethodPost)
 	}
-	api.HandleFunc("/settings", SettingsHandler(s)).Methods("GET", "POST")
-	api.HandleFunc("/backup", BackupHandler(s)).Methods("POST")
-	api.HandleFunc("/transactions/getcount", TransactionsCountHandler(s)).Methods("POST")
-	api.HandleFunc("/transactions/getpage", TransactionsHandler(s)).Methods("POST")
-	api.HandleFunc("/transaction/{id}", TransactionHandler(s)).Methods("GET", "POST", "DELETE")
-	api.HandleFunc("/report", ReportHandler(s)).Methods("POST")
-	api.HandleFunc("/accounts", AccountsHandler(s)).Methods("GET")
-	api.HandleFunc("/account/{id}", AccountHandler(s)).Methods("GET", "POST", "DELETE")
-	api.HandleFunc("/tags", TagsHandler(s)).Methods("GET")
+	api.HandleFunc("/settings", SettingsHandler(s)).Methods(http.MethodGet, http.MethodPost)
+	api.HandleFunc("/backup", BackupHandler(s)).Methods(http.MethodPost)
+	api.HandleFunc("/transactions/getcount", TransactionsCountHandler(s)).Methods(http.MethodPost)
+	api.HandleFunc("/transactions/getpage", TransactionsHandler(s)).Methods(http.MethodPost)
+	api.HandleFunc("/transaction/{id}", TransactionHandler(s)).Methods(http.MethodGet, http.MethodPost, http.MethodDelete)
+	api.HandleFunc("/report", ReportHandler(s)).Methods(http.MethodPost)
+	api.HandleFunc("/accounts", AccountsHandler(s)).Methods(http.MethodGet)
+	api.HandleFunc("/account/{id}", AccountHandler(s)).Methods(http.MethodGet, http.MethodPost, http.MethodDelete)
+	api.HandleFunc("/tags", TagsHandler(s)).Methods(http.MethodGet)
 	return r, nil
 }
